1852088d39fe36dfd024936f6340c0c68d98c84d
Gwendolyn <wilkistagwendolyn@gmail.com>
2025-06-08 15:06:21 +0300

    Importing google fonts

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,8 +1,9 @@
-export default function Home() {
+const Home = ()=> {
   return (
     <div className="flex flex-col">
-      <h1 className="text-4xl font-bold text-center mt-10">Welcome to the Splash App!</h1>
-      
+      <h1 className="text-4xl font-bold text-center mt-10"> Welcome to Splash App </h1>
     </div>
   );
 }
+
+export default Home;
