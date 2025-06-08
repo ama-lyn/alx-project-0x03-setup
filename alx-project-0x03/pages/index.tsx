6b9a6999948c9dae13df4238ef133c1ec6c2c323
Gwendolyn <wilkistagwendolyn@gmail.com>
2025-06-08 14:58:44 +0300

    Implementing shared layout

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-4xl font-bold text-center mt-10">Welcome to the Splash App!</h1>
+      
+    </div>
+  );
+}
